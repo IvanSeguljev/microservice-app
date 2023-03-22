9ec6b2301780001a8ebb549236eb1f96217251aa
Ivan <ivan.seguljev@yahoo.com>
2023-03-22 11:47:17 +0100

    Added product service

---
+++ product-service/src/main/java/com/iseguljev/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iseguljev.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
